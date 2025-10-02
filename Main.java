7ba6d0e58d3915ab9f88a87f749e040646679d5b
Sephagus <sethmcbee@outlook.com>
2025-10-02 10:05:41 -0400

    Simplified getting alphabet value from next char in file

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.*;
+
+//TODO: add all states
 
 enum State {
     q0(null), //start
@@ -12,14 +13,14 @@ enum State {
     q4(null),;
 
     public final String token;
-    State(String label) {
-        this.token = label;
+    State(String token) {
+        this.token = token;
     }
 }
 
 
 enum InputAlphabet {
-    VARIABLE_NAME,
+    IDENTIFIER,
     INTEGER_LITERAL,
     FLOAT_LITERAL,
     WHILE,
@@ -34,67 +35,94 @@ enum InputAlphabet {
     GREATER_THAN_OR_EQUALS,
     LEFT_PAREN,
     RIGHT_PAREN,
-    EL,
     ADD,
     SUB,
     MUL,
     DIV,
     POINT,
     EOL,
-    EOF,
     INVALID
 }
 
 public class Main {
-    static Pattern variableName =  Pattern.compile("^[a-zA-Z]\\w*");
-    static Pattern matchDigit = Pattern.compile("\\d");
-    static Pattern matchFloat = Pattern.compile("^\\.\\d+");
-
     static StringBuilder number = new StringBuilder();
+    static StringBuilder identifier = new StringBuilder();
+
     static int nextChar;
 
-    static InputAlphabet analyzeInput(FileReader fr) throws IOException {
-        StringBuilder sb = new StringBuilder();
+    static void getNext(FileReader fr) throws IOException {
         nextChar = fr.read();
         if (nextChar == -1) {
-            return InputAlphabet.EOF;
+            System.out.println("End of file");
+            System.exit(0);
         }
-        while (nextChar != ' ' && nextChar != '\t') {
-            sb.append((char) nextChar);
-            //handle compound tokens and end of line
-            String current = sb.toString();
-            switch (current) {
-                case String s when matchDigit.matcher(s).find() -> {
-                    number.append(current);
-                    return InputAlphabet.INTEGER_LITERAL;
-                }
-                case ">" -> {return InputAlphabet.GREATER_THAN;}
-                case "<" -> {return InputAlphabet.LESS_THAN;}
-                case "=" -> {return InputAlphabet.ASSIGN;}
-                case "\r\n", "\n" -> {return InputAlphabet.EOL;}
-                default -> {}
-            }
-            nextChar = fr.read();
-        }
-        String current = sb.toString();
-        return switch (current) {
-            case "while" -> InputAlphabet.WHILE;
-            case "for" -> InputAlphabet.FOR;
-            case "if" -> InputAlphabet.IF;
-            case "(" -> InputAlphabet.LEFT_PAREN;
-            case ")" -> InputAlphabet.RIGHT_PAREN;
-            case "+" -> InputAlphabet.ADD;
-            case "-" -> InputAlphabet.SUB;
-            case "*" -> InputAlphabet.MUL;
-            case "/" -> InputAlphabet.DIV;
-            case String s when variableName.matcher(s).find() -> InputAlphabet.VARIABLE_NAME;
-            default -> InputAlphabet.INVALID;
-        };
-
     }
 
-    // [state][value]
-    List<State>[][] transitions = new List[23][23];
+    static InputAlphabet analyzeInput(FileReader fr) throws IOException {
+
+        getNext(fr);
+
+        char current = (char)nextChar;
+        switch (current) {
+            case '(' -> {return InputAlphabet.LEFT_PAREN;}
+            case ')' -> {return InputAlphabet.RIGHT_PAREN;}
+            case '+' -> {return InputAlphabet.ADD;}
+            case '-' -> {return InputAlphabet.SUB;}
+            case '*' -> {return InputAlphabet.MUL;}
+            case '/' -> {return InputAlphabet.DIV;}
+            case '>' -> {return InputAlphabet.GREATER_THAN;}
+            case '<' -> {return InputAlphabet.LESS_THAN;}
+            case '=' -> {return InputAlphabet.ASSIGN;}
+            case '.' -> {return InputAlphabet.POINT;}
+            case ' ', '\t' -> {return InputAlphabet.WHITESPACE;}
+            case '\n' -> {return InputAlphabet.EOL;} // if this gets run on linux for whatever reason
+            case '\r' -> {
+                fr.read(); //consume newline character
+                return InputAlphabet.EOL;
+            }
+            case '_' -> {
+                if (identifier.isEmpty()) {
+                    return InputAlphabet.INVALID;
+                } else  {
+                    identifier.append(current);
+                    return InputAlphabet.IDENTIFIER;
+                }
+            }
+            default -> {}
+        }
+        if (Character.isDigit(current)) {
+            if (identifier.isEmpty()) {
+                number.append(current);
+                return InputAlphabet.INTEGER_LITERAL;
+            } else {
+                identifier.append(current);
+                return InputAlphabet.IDENTIFIER;
+            }
+        } else if (Character.isLetter(current)) {
+            identifier.append(current);
+            return InputAlphabet.IDENTIFIER;
+        } else {
+            return InputAlphabet.INVALID;
+        }
+    }
+
+    List<State>[][] transitions = new List[State.values().length][InputAlphabet.values().length];
+    // positions in the table can be accessed with enum ordinals
+    // a warning may be thrown when accessing ordinal 0 don't worry about it
+    State testState = State.q1;
+    int ordinal = testState.ordinal();
+    InputAlphabet testInput = InputAlphabet.ADD;
+    int ordinal2 = testInput.ordinal();
+    // accessing token for a given state, null if transition state
+    String token = testState.token;
+
+    /*
+        TODO: create transition table proper
+            MAKE SURE TO PUT STATES IN THEIR PROPER POSITIONS
+            IF YOU DON'T IT WON'T WORK
+            Make a factory for cells in the transition table probably
+     */
+
 
     public static void main(String[] args) {
         System.out.println("CS-410 Compiler");
@@ -111,9 +139,19 @@ public class Main {
 
         int q_index = 0;
         int s_index = 0;
+        State currentState = State.q0;
+        InputAlphabet nextInput;
 
         try (FileReader fr = new FileReader(file.getAbsolutePath())) {
-
+            while(true) {
+                nextInput = analyzeInput(fr);
+                /*
+                TODO: Process current state
+                    Use ordinal of current state and ordinal of input to find next state in transition table
+                    Create special case for keywords
+                    Reset on whitespace/EOL and print out last token
+                 */
+            }
         } catch (IOException e) {
             System.out.println("File not found: " + sourceFile);
         }
